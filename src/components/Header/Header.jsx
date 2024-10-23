8623f31c481663e6646047d6da21eb78c7489c8d
fstone <164958630+fstone-codes@users.noreply.github.com>
2024-10-22 20:07:29 -0400

    link header images, add header attributes and styles per mockup

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,29 @@
 import "./Header.scss";
+import logo from "../../assets/logos/Brainflix-logo.svg";
+import avatar from "../../assets/images/Mohan-muruge.jpg";
+import search from "../../assets/icons/search.svg";
+import upload from "../../assets/icons/upload.svg";
 
 function Header() {
     return (
-        <header>
-            <a>
-                <img />
+        <header className="header">
+            <a className="header__link" href="./index.html">
+                <img className="header__logo" src={logo} alt="BrainFlix logo" />
             </a>
-            <form>
-                <input></input>
-                <div>
-                    <img />
+            <form className="header__form">
+                <div className="header__container">
+                    <input className="header__input" placeholder="Search"></input>
+                    <div className="header__image-container header__image-container--between">
+                        <img className="header__image" src={avatar} />
+                    </div>
                 </div>
-                <button></button>
+                <button className="btn">
+                    <img className="btn__image" src={upload} />
+                    UPLOAD
+                </button>
             </form>
-            <div>
-                <img />
+            <div className="header__image-container header__image-container--after">
+                <img className="header__image" src={avatar} />
             </div>
         </header>
     );
