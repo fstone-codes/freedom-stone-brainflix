fe49a43e8422f7f5cca39ba7d78f87aa30f1d23c
fstone <164958630+fstone-codes@users.noreply.github.com>
2024-10-22 20:27:55 -0400

    add video element attributes and style per mockup, add img element as placeholder

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -1,7 +1,11 @@
 import "./Video.scss";
+import placeholder from "../../assets/images/Upload-video-preview.jpg";
 
 function Video() {
-    return <video></video>;
+    return <img className="video" src={placeholder} />;
+    {
+        /* <video className="video" autoPlay muted controls poster={placeholder}></video> */
+    }
 }
 
 export default Video;
