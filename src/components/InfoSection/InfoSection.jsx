99faa922c55d6185afe8a568e80a88bec0aa7018
fstone <164958630+fstone-codes@users.noreply.github.com>
2024-10-23 09:07:09 -0400

    edit index.html head title and add function to convert timestamp per mockup

--- src/components/InfoSection/InfoSection.jsx
+++ src/components/InfoSection/InfoSection.jsx
@@ -3,13 +3,24 @@ import viewIcon from "../../assets/icons/views.svg";
 import likeIcon from "../../assets/icons/likes.svg";
 
 function InfoSection({ title, channel, timestamp, views, likes, description }) {
+    function convertTime(timestamp) {
+        let submissionDate = new Date(timestamp);
+        let formattedDate = submissionDate.toLocaleDateString("en-US", {
+            year: "numeric",
+            month: "numeric",
+            day: "numeric",
+        });
+
+        return formattedDate;
+    }
+
     return (
         <section className="info">
             <h1 className="info__title">{title}</h1>
             <div className="info__container">
                 <div className="info__data-container">
                     <p className="info__data">By {channel}</p>
-                    <p className="info__data">{timestamp}</p>
+                    <p className="info__data">{convertTime(timestamp)}</p>
                 </div>
                 <div className="info__data-container">
                     <p className="info__data">
