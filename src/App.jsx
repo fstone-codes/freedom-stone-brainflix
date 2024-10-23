579c3e1d6b7e276d88ebec73ae14363971c0b10a
fstone <164958630+fstone-codes@users.noreply.github.com>
2024-10-22 21:18:10 -0400

    add info section attributes, add styles and create temporary props linking to JSON data file for content

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Video from "./components/Video/Video";
 import InfoSection from "./components/InfoSection/InfoSection";
 import CommentSection from "./components/CommentSection/CommentSection";
 import QueueSection from "./components/QueueSection/QueueSection";
+import videoDetails from "./data/video-details.json";
 
 function App() {
     return (
@@ -11,7 +12,14 @@ function App() {
             <Header />
             <main>
                 <Video />
-                <InfoSection />
+                <InfoSection
+                    title={videoDetails[0].title}
+                    channel={videoDetails[0].channel}
+                    timestamp={videoDetails[0].timestamp}
+                    views={videoDetails[0].views}
+                    likes={videoDetails[0].likes}
+                    description={videoDetails[0].description}
+                />
                 <CommentSection />
                 <QueueSection />
             </main>
--- src/components/InfoSection/InfoSection.jsx
+++ src/components/InfoSection/InfoSection.jsx
@@ -1,20 +1,28 @@
 import "./InfoSection.scss";
+import viewIcon from "../../assets/icons/views.svg";
+import likeIcon from "../../assets/icons/likes.svg";
 
-function InfoSection() {
+function InfoSection({ title, channel, timestamp, views, likes, description }) {
     return (
-        <section>
-            <h1></h1>
-            <div>
-                <div>
-                    <p></p>
-                    <p></p>
+        <section className="info">
+            <h1 className="info__title">{title}</h1>
+            <div className="info__container">
+                <div className="info__data-container">
+                    <p className="info__data">By {channel}</p>
+                    <p className="info__data">{timestamp}</p>
                 </div>
-                <div>
-                    <p></p>
-                    <p></p>
+                <div className="info__data-container">
+                    <p className="info__data">
+                        <img className="info__icon" src={viewIcon} />
+                        {views}
+                    </p>
+                    <p className="info__data">
+                        <img className="info__icon" src={likeIcon} />
+                        {likes}
+                    </p>
                 </div>
             </div>
-            <p></p>
+            <p className="info__description">{description}</p>
         </section>
     );
 }
