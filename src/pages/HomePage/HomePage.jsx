901f3a6ceb407dae8956d75f40f45cc6ecdb335a
fstone <164958630+fstone-codes@users.noreply.github.com>
2024-10-28 21:18:25 -0400

    fetch data through axios request

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,27 @@
 import "./HomePage.scss";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { baseUrl, apiKey } from "../../utils";
 import Main from "../../components/Main/Main";
 
 function HomePage({ videos, currentVideo, handleClick }) {
+    const [videoData, setVideoData] = useState(null);
+
+    const getVideoData = async () => {
+        try {
+            const { data } = await axios.get(`${baseUrl}/videos?api_key=${apiKey}`);
+            setVideoData(data);
+        } catch (error) {
+            console.error("You have an error:", error);
+        }
+    };
+    console.log(videoData);
+
+    useEffect(() => {
+        getVideoData();
+        console.log(videoData);
+    }, []);
+
     return <Main currentVideo={currentVideo} videos={videos} />;
 }
 
